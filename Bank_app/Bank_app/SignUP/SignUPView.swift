abcefc38499342fe2ea24d4f74a046d07500f8a5
Rezwan Chowdhury <tipu.diu.cse@gmail.com>
2024-08-07 11:13:16 +0600

    picker added in signup
    picker added in signup

--- Bank_app/Bank_app/SignUP/SignUPView.swift
+++ Bank_app/Bank_app/SignUP/SignUPView.swift
@@ -9,12 +9,35 @@ import SwiftUI
 
 struct SignUPView: View {
     @Environment(\.presentationMode) var presentationMode
+    @State private var selectedAccountType: AccountType = .regular
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationTitle("Sign Up")
-            .navigationBarBackButtonHidden(true)
-            .navigationBarItems(leading: backButton)
+        VStack {
+            Picker("Account Type", selection: $selectedAccountType) {
+                ForEach(AccountType.allCases, id: \.self) { type in
+                    Text(type.rawValue.capitalized)
+                        .tag(type)
+                }
+            }
+            
+            .pickerStyle(SegmentedPickerStyle())
+            .frame(height: 45)
+            .scaleEffect(CGSize(width: 1, height: 1.2))
+            .scaledToFit()
+            .padding(8)
+            .background(Color(UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1.00)))
+            .cornerRadius(15)
+            .padding()
+            
+            // Additional content can go here, based on the selected account type
+            Text("Selected account type: \(selectedAccountType.rawValue.capitalized)")
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Sign Up")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
     }
     
     private var backButton: some View {
@@ -30,7 +53,11 @@ struct SignUPView: View {
             }
         }
     }
-    
+}
+
+enum AccountType: String, CaseIterable {
+    case regular
+    case propertoship
 }
 
 #Preview {
